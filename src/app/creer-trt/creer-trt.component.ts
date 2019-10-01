e8304adf21bb925058bf7adeeb32379043db8d21
rémi <rcamberlein@gmail.com>
2019-10-01 17:22:29 +0200

    #4382 : mise en forme formulaire création

--- src/app/creer-trt/creer-trt.component.ts
+++ src/app/creer-trt/creer-trt.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Traitement } from '../traitement-item';
+/*import { DatePipe } from '@angular/common';*/
 
 @Component({
   selector: 'app-creer-trt',
@@ -11,20 +12,26 @@ export class CreerTrtComponent implements OnInit {
 	nom: string;
 	desc: string;
 	type: number;
+	type_text: string;
+	list_type = ['enregistrement', 'modification', 'suppression'];
+	dateCreation: Date = new Date();
+	dateModif: Date;
+	dateSuppr: Date;
+	data: string;
+	resp_trt: string;
+	resp_service: string;
+	dpo: string;
+	finalites: string;
+	acteurs: string;
+	securite: string;
+	hebergement: string;
+	hors_ue: boolean;
 
   list_trt : Traitement[] = [
-  /*
-    {nom: 'test', desc: '', type: 1, dateCreation: new Date(), dateModif: null, dateSuppr: null},
+  
+    /*{nom: 'test', desc: '', type: 1, dateCreation: new Date(), dateModif: null, dateSuppr: null},
     {nom: 'test2', desc: '', type: 1, dateCreation: new Date(), dateModif: null, dateSuppr: null}*/
-    {nom: 'test', desc: '', type: 1}
   ];
-
-	/*nom: string;
-	description: string;
-	type: number;
-	dateCreation: Date;
-	dateModif: Date;
-	dateSuppr: Date;*/
 
   constructor() {
   	/*this.new_trt.dateCreation = new Date();*/
@@ -34,8 +41,41 @@ export class CreerTrtComponent implements OnInit {
   }
 
   save() : void{
-    let trt = new Traitement(this.nom);
-    trt.desc = this.desc
-    this.list_trt.push(trt);
+
+
+  		window.alert('save')
+  	if (!(this.nom.length>0 ||
+  		this.desc.length>0 ||
+  		this.type_text.length>0 ||
+  		this.dateCreation != null ||
+  		this.data.length>0 ||
+  		this.resp_trt.length>0 ||
+  		this.resp_service.length>0 ||
+  		this.dpo.length>0 ||
+  		this.finalites.length>0 ||
+  		this.acteurs.length>0 ||
+  		this.securite.length>0 ||
+  		this.finalites.length>0 ||
+  		this.hebergement.length>0 )) {
+  		window.alert('Tous les champs doivent être renseignés pour valider la soumission du traitement')
+  	} else {
+
+	    let trt = new Traitement(this.nom);
+	    trt.desc = this.desc
+	    trt.type_text = this.type_text
+	    trt.dateCreation = this.dateCreation
+	    trt.data = this.data
+	    trt.resp_trt = this.resp_trt
+	    trt.resp_service = this.resp_service
+	    trt.dpo = this.dpo
+	    trt.finalites = this.finalites
+	    trt.acteurs = this.acteurs
+	    trt.securite = this.securite
+	    trt.finalites = this.finalites
+	    trt.hebergement = this.hebergement
+	    trt.hors_ue = this.hors_ue;
+
+	    this.list_trt.push(trt);
+  	}
   }
 }
--- src/app/traitement-item.ts
+++ src/app/traitement-item.ts
@@ -1,10 +1,20 @@
 export class Traitement {
-  nom : string;
-  desc : string;
-  type : number;
-  /*dateCreation : Date;
-  dateModif : Date;
-  dateSuppr : Date;*/
+  	nom : string;
+  	desc : string;
+  	type : number;
+  	type_text : string;
+  	dateCreation : Date;
+  	dateModif : Date;
+  	dateSuppr : Date;
+	data: string;
+	resp_trt: string;
+	resp_service: string;
+	dpo: string;
+	finalites: string;
+	acteurs: string;
+	securite: string;
+	hebergement: string;
+	hors_ue: boolean;
 
   constructor(nom: string){
     this.nom=nom;
